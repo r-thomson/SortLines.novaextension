dc624d615566b120ff9d9b3dfdf997528774eac2
Ryan Thomson <git@ryanthomson.net>
2024-01-16 20:42:35 -0500

    Set up empty commands

---
+++ Scripts/main.js
@@ -0,0 +1,5 @@
+nova.commands.register('SortLines.sortLinesAsc', (workspace) => {});
+
+nova.commands.register('SortLines.sortLinesDesc', (workspace) => {});
+
+nova.commands.register('SortLines.reverseLines', (workspace) => {});
