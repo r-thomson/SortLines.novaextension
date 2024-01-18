34f6bae6a221bfbd75b00509113c4ac1a153d0b7
Ryan Thomson <git@ryanthomson.net>
2024-01-17 22:22:54 -0500

    Add config options

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,24 @@
 /** Matches LF, CR, or CRLF */
 const NEWLINE_REGEX = /\n|\r\n|\r/;
 
+const NovaConfig = {
+	/** @type {boolean} */
+	get ignoreWhitespace() {
+		return nova.config.get('SortLines.ignoreWhitespace', 'bool');
+	},
+	/** @type {"lowerFirst" | "upperFirst"} */
+	get caseOrder() {
+		return nova.config.get('SortLines.caseOrder', 'string');
+	},
+	/** @type {boolean} */
+	get useNumericCollation() {
+		return nova.config.get('SortLines.useNumericCollation', 'bool');
+	},
+};
+
 nova.commands.register('SortLines.sortLinesAsc', (editor) => {
+	const compare = getCompareFn();
+
 	editor.edit((textEdit) => {
 		for (const range of editor.selectedRanges) {
 			const lineRange = editor.getLineRangeForRange(range);
@@ -12,9 +29,7 @@ nova.commands.register('SortLines.sortLinesAsc', (editor) => {
 			// After the split, there will be one empty string ('') at the end of the array
 			// because of the final newline. Ensure it stays at the end of the array.
 			lines.pop();
-			lines.sort((lhs, rhs) =>
-				lhs.localeCompare(rhs, undefined, { sensitivity: 'base' }),
-			);
+			lines.sort((lhs, rhs) => compare(lhs, rhs));
 			lines.push('');
 
 			textEdit.replace(
@@ -27,6 +42,8 @@ nova.commands.register('SortLines.sortLinesAsc', (editor) => {
 });
 
 nova.commands.register('SortLines.sortLinesDesc', (editor) => {
+	const compare = getCompareFn();
+
 	editor.edit((textEdit) => {
 		for (const range of editor.selectedRanges) {
 			const lineRange = editor.getLineRangeForRange(range);
@@ -35,9 +52,7 @@ nova.commands.register('SortLines.sortLinesDesc', (editor) => {
 			const lines = editor.getTextInRange(lineRange).split(NEWLINE_REGEX);
 
 			lines.pop();
-			lines.sort((lhs, rhs) =>
-				lhs.localeCompare(rhs, undefined, { sensitivity: 'base' }),
-			);
+			lines.sort((lhs, rhs) => -compare(lhs, rhs));
 			lines.push('');
 
 			textEdit.replace(
@@ -69,3 +84,19 @@ nova.commands.register('SortLines.reverseLines', (editor) => {
 		}
 	});
 });
+
+function getCompareFn() {
+	const { caseOrder, ignoreWhitespace, useNumericCollation } = NovaConfig;
+
+	const collator = Intl.Collator(undefined, {
+		caseFirst: caseOrder === 'upperFirst' ? 'upper' : 'lower',
+		numeric: useNumericCollation,
+		sensitivity: 'case',
+	});
+
+	return (lhs, rhs) =>
+		collator.compare(
+			ignoreWhitespace ? lhs.trimStart() : lhs,
+			ignoreWhitespace ? rhs.trimStart() : rhs,
+		);
+}
