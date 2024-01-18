f3361d371d363c611aca0b8d281ddfbc4aa7ea96
Ryan Thomson <git@ryanthomson.net>
2024-01-18 18:37:21 -0500

    Extract common command operations

--- Scripts/commands.js
+++ Scripts/commands.js
@@ -16,75 +16,10 @@ const NovaConfig = {
 	},
 };
 
-exports.sortLinesAscCommand = function (editor) {
-	const compare = getCompareFn();
-
-	editor.edit((textEdit) => {
-		for (const range of editor.selectedRanges) {
-			const lineRange = editor.getLineRangeForRange(range);
-
-			/** @type {string[]} */
-			const lines = editor.getTextInRange(lineRange).split(NEWLINE_REGEX);
-
-			// After the split, there will be one empty string ('') at the end of the array
-			// because of the final newline. Ensure it stays at the end of the array.
-			lines.pop();
-			lines.sort((lhs, rhs) => compare(lhs, rhs));
-			lines.push('');
-
-			textEdit.replace(
-				lineRange,
-				lines.join(editor.document.eol),
-				InsertTextFormat.PlainText,
-			);
-		}
-	});
-};
-
-exports.sortLinesDescCommand = function (editor) {
-	const compare = getCompareFn();
-
-	editor.edit((textEdit) => {
-		for (const range of editor.selectedRanges) {
-			const lineRange = editor.getLineRangeForRange(range);
-
-			/** @type {string[]} */
-			const lines = editor.getTextInRange(lineRange).split(NEWLINE_REGEX);
-
-			lines.pop();
-			lines.sort((lhs, rhs) => -compare(lhs, rhs));
-			lines.push('');
-
-			textEdit.replace(
-				lineRange,
-				lines.join(editor.document.eol),
-				InsertTextFormat.PlainText,
-			);
-		}
-	});
-};
-
-exports.reverseLinesCommand = function (editor) {
-	editor.edit((textEdit) => {
-		for (const range of editor.selectedRanges) {
-			const lineRange = editor.getLineRangeForRange(range);
-
-			/** @type {string[]} */
-			const lines = editor.getTextInRange(lineRange).split(NEWLINE_REGEX);
-
-			lines.pop();
-			lines.reverse();
-			lines.push('');
-
-			textEdit.replace(
-				lineRange,
-				lines.join(editor.document.eol),
-				InsertTextFormat.PlainText,
-			);
-		}
-	});
-};
-
+/**
+ * Make a `.sort()` compare function based on the user's preferences.
+ * @returns {(a: string, b: string) => number}
+ */
 function getCompareFn() {
 	const { caseOrder, ignoreWhitespace, useNumericCollation } = NovaConfig;
 
@@ -100,3 +35,52 @@ function getCompareFn() {
 			ignoreWhitespace ? rhs.trimStart() : rhs,
 		);
 }
+
+/**
+ * Perform an edit on the selected lines. `callback` should mutate the array in-place.
+ * @param callback {(lines: string[]) => void}
+ */
+function performLineEdit(editor, callback) {
+	editor.edit((textEdit) => {
+		for (const range of editor.selectedRanges) {
+			const lineRange = editor.getLineRangeForRange(range);
+
+			/** @type {string[]} */
+			const lines = editor.getTextInRange(lineRange).split(NEWLINE_REGEX);
+
+			// After the split, there will be one empty string ('') at the end of the array
+			// because of the final newline. Ensure it stays at the end of the array.
+			lines.pop();
+			callback(lines);
+			lines.push('');
+
+			textEdit.replace(
+				lineRange,
+				lines.join(editor.document.eol),
+				InsertTextFormat.PlainText,
+			);
+		}
+	});
+}
+
+exports.sortLinesAscCommand = function (editor) {
+	const compare = getCompareFn();
+
+	performLineEdit(editor, (lines) => {
+		lines.sort((lhs, rhs) => compare(lhs, rhs));
+	});
+};
+
+exports.sortLinesDescCommand = function (editor) {
+	const compare = getCompareFn();
+
+	performLineEdit(editor, (lines) => {
+		lines.sort((lhs, rhs) => -compare(lhs, rhs));
+	});
+};
+
+exports.reverseLinesCommand = function (editor) {
+	performLineEdit(editor, (lines) => {
+		lines.reverse();
+	});
+};
